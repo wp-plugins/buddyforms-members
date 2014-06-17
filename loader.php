d1ece976c3aeb94d2620eafee188e2054a4d6a08
svenl77 <svenl77@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-17 10:56:14 +0000

    new version 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/buddyforms-members/tags/1.0.3@933613 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ loader.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ Plugin Name: BuddyForms Members
+ Plugin URI: http://buddyforms.com
+ Description: The BuddyForms Members Component. Let your members write right out of their profiles.
+ Version: 1.0.3
+ Author: Sven Lehnert
+ Author URI: http://themekraft.com/members/svenl77/
+ License: GPLv2 or later
+ Network: false
+
+ *****************************************************************************
+ *
+ * This script is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ ****************************************************************************
+ */
+
+/**
+ * Loads BuddyForms files only if BuddyPress is present
+ *
+ * @package BuddyForms
+ * @since 0.1 beta
+ */
+
+
+function buddyforms_members_requirements(){
+
+    // Needs to be rewritetn in Otto style ;-)
+    if( ! defined( 'BP_VERSION' )){
+        add_action( 'admin_notices', create_function( '', 'printf(\'<div id="message" class="error"><p><strong>\' . __(\'BuddyForms Members needs BuddyPress to be installed. <a href="%s">Download it now</a>!\', " buddyforms" ) . \'</strong></p></div>\', admin_url("plugin-install.php") );' ) );
+        return;
+    }
+
+    if( ! defined( 'buddyforms' )){
+        add_action( 'admin_notices', create_function( '', 'printf(\'<div id="message" class="error"><p><strong>\' . __(\'BuddyForms Members needs BuddyForms to be installed. <a href="%s" target="_new">Download it now</a>!\', " buddyforms" ) . \'</strong></p></div>\',  "http://buddyforms.com" );' ) );
+        return;
+    }
+
+}
+
+add_action('plugins_loaded', 'buddyforms_members_requirements');
+
+function buddyforms_members_init() {
+	global $wpdb, $buddyforms_members;
+
+    define('buddyforms_members', '1.0.3');
+
+	if (is_multisite() && BP_ROOT_BLOG != $wpdb->blogid)
+		return;
+
+	require (dirname(__FILE__) . '/buddyforms-members.php');
+	$buddyforms_members = new BuddyForms_Members();
+
+}
+
+add_action('bp_loaded', 'buddyforms_members_init');
